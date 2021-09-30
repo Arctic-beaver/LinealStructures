54ab755dea7f6056339290c9c0c77aeec593eaaf
Alice Palermo <petrowa.aliska@yandex.ru>
2021-09-30 14:30:51 +0300

    Classes Node and List completed

--- ListFolder/ListClass.cs
+++ ListFolder/ListClass.cs
@@ -6,7 +6,87 @@ using System.Threading.Tasks;
 
 namespace Lab_1_lineal
 {
-    class ListClass
+    public class ListClass
     {
+        static Node head;
+        static int amount_of_nodes = 0;
+
+        public Node GetHead()
+        {
+            return head;
+        }
+
+        public int GetAmountOfNodes()
+        {
+            return amount_of_nodes;
+        }
+
+        public Node GetLast()
+        {
+            Node shovel = head;
+            while (shovel.GetNext() != null)
+            {
+                shovel = shovel.GetNext();
+            }
+            return shovel;
+        }
+        public Node GetOnPos(int position)
+        {
+            Node shovel = head;
+            for (int i = 0; i <= position; i++)
+            {
+                shovel = shovel.GetNext();
+            }
+            return shovel;
+        }
+
+        public void CreateNode(Node previous, int data)
+        {
+            //to the end or center
+            Node new_node = new Node();
+            new_node.SetData(data);
+            new_node.SetNext(previous.GetNext());
+            previous.SetNext(new_node);
+        }
+
+        public void CreateNode(int data)
+        {
+            //to head
+            Node new_node = new Node();
+            new_node.SetData(data);
+
+            new_node.SetNext(head.GetNext());
+            head.SetNext(new_node);
+        }
+
+        public void Add(int data, int position)
+        {
+            if (position == 0)
+            {
+                //to the head
+                CreateNode(data);
+            }
+            else
+            {
+                CreateNode(GetOnPos(position - 1), data);
+            }
+            amount_of_nodes += 1;
+        }
+
+        public void Delete(int position)
+        {
+            Node shovel = head;
+            //find previous;
+            for (int i = 0; i < position; i++)
+            {
+                shovel = shovel.GetNext();
+            }
+            shovel.SetNext(shovel.GetNext().GetNext());
+        }
+
+        public void Free()
+        {
+            head = null;
+        }
     }
 }
---
+++ ListFolder/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_lineal
+{
+    public class Node
+    {
+        int data;
+        Node next = null;
+
+        public void SetNext(Node next)
+        {
+            this.next = next;
+        }
+
+        public void SetData(int data)
+        {
+            this.data = data;
+        }
+
+        public Node GetNext()
+        {
+            return this.next;
+        }
+        public int GetData()
+        {
+            return this.data;
+        }
+    }
+}
